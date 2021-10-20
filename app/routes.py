0fcb0fb9b4bc23923a9977a50b440277c88377ce
domittaa <klodzinska.d@gmail.com>
2021-10-20 12:48:19 +0200

    login

--- app/routes.py
+++ app/routes.py
@@ -2,26 +2,43 @@ from werkzeug.security import generate_password_hash
 from app import app, db
 from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm, RegisterForm
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 
 
 @app.route('/', methods=['GET'])
-def hello_world():  # put application's code here
+@login_required
+def index():  # put application's code here
     return render_template('index.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
     return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
-        new_user = User(username=form.username.data, email=form.email.data,
-                        password_hash=generate_password_hash(form.password.data, method='sha256'))
+        new_user = User(username=form.username.data, email=form.email.data)
+        new_user.set_password(form.password.data)
         db.session.add(new_user)
         db.session.commit()
         flash("You are now registered user. Please login.")
