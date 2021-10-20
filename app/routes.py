48679a7a3ffc4939b3ae5aadb9d8340ca33bc9f2
domittaa <klodzinska.d@gmail.com>
2021-10-20 08:13:56 +0200

    first form

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,14 @@
 from app import app
+from flask import render_template
+from app.forms import LoginForm
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
